ae596e31c11bd1cbf43e459527429ad05f2cb35c
Allen Grimm <allen@grimmscience.com>
2016-07-08 17:44:51 -0700

    updating comments in designate_edge_type.

--- Automated_Design/designate_edge_type.py
+++ Automated_Design/designate_edge_type.py
@@ -2,15 +2,14 @@ from networkx.algorithms.mst import prim_mst
 
 def designate_edge_type(full_graph):
     """
-    ...
-    1 is non-spanning tree edge: DX edge with 1 scaffold crossover
-    2 is spanning tree edge: DX edge with 0 scaffold crossovers
+    Calculate minimum spanning tree, and label the links in the full graph that are also in the min spanning tree as such:
+        1 is non-spanning tree edge: DX edge with 1 scaffold crossover
+        2 is spanning tree edge: DX edge with 0 scaffold crossovers
     """
     tree = prim_mst(full_graph)
 
     full_tree = tree #networkx undirected networks already implicitly can be considered a symmetrical matrix
 
-    #TODO: is `edge_type_mat = full_tree + full_graph` a no-op?  if not, what's the equivalent in this representation?
     tree_edges = full_tree.edges()
     for edge in full_graph.edges():
         print edge
