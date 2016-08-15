7598ade10ccabc9bd9e2498a6e9bbc6f8e8ed805
Allen Grimm <TheGrimmScientist@gmail.com>
2016-08-15 16:23:32 -0700

    Beginnings of testing infrastructure.

---
+++ test.py
@@ -0,0 +1,8 @@
+from unittest import main
+
+# Import all tests here using `import *` to put test classes into this namespace
+from tests.test_dx_cage_design import *
+
+
+if __name__ == '__int__':
+    main()
