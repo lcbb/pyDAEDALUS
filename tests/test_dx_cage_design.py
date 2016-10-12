e970475271a81afea70bb17c46977763740122d7
Allen Grimm <allen@grimmscience.com>
2016-10-12 00:56:14 -0700

    Fixing how two tests call ply_to_input to match changes from a few commits ago.

--- tests/test_dx_cage_design.py
+++ tests/test_dx_cage_design.py
@@ -24,14 +24,14 @@ from tests.utils import open_string_as_file
 class TestPlyImport(TestCase):
     def test_01_tetrahedron(self):
         f = open_string_as_file(ply_file_01_tetrahedron)
-        coordinates, edges, faces, edge_length_vec, file_name, staple_name, singleXOs = ply_to_input("01_tetrahedron", f, min_len_nt=52)
+        coordinates, edges, faces, edge_length_vec, file_name, staple_name, singleXOs = ply_to_input("01_tetrahedron", f, min_len_nt=52, results_foldername=None)
         self.assertEqual(len(coordinates), 4)
         self.assertEqual(len(faces), 4)
         #TODO: improve these assertions
 
     def test_05_icosahedron(self):
         f = open_string_as_file(ply_file_05_icosahedron)
-        coordinates, edges, faces, edge_length_vec, file_name, staple_name, singleXOs = ply_to_input("05_tetrahedron", f, min_len_nt=52)
+        coordinates, edges, faces, edge_length_vec, file_name, staple_name, singleXOs = ply_to_input("05_tetrahedron", f, min_len_nt=52, results_foldername=None)
         self.assertEqual(len(coordinates), 12)
         self.assertEqual(len(faces), 20)
         # TODO: improve these assertions
