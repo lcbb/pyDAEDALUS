2466a6e2e8ddc549f94ef355e8a155ee09bf0ec1
Allen Grimm <TheGrimmScientist@gmail.com>
2016-12-26 15:12:15 -0500

    Fixing issue with mocking by converting to dependency injection.

--- tests/test_dx_cage_design.py
+++ tests/test_dx_cage_design.py
@@ -25,22 +25,21 @@ from tests.target_parsers import load_mat_file, load_graph_from_mat, \
 from tests.utils import open_string_as_file
 
 
-@patch('Automated_Design.plotters.plot_edge_length_distributions')
-def test_ply_input_for_01_tetrahedron(savefig_mock):
+
+def test_ply_input_for_01_tetrahedron():
     f = open_string_as_file(ply_file_01_tetrahedron)
     coordinates, edges, faces, edge_length_vec, file_name, \
         staple_name, singleXOs = ply_to_input(
-            "01_tetrahedron", f, min_len_nt=52)
+            "01_tetrahedron", f, min_len_nt=52, plot=False)
     assert len(coordinates) == 4
     assert len(faces) == 4
 
 
-@patch('Automated_Design.plotters.plot_edge_length_distributions')
-def test_ply_input_for_05_icosahedron(savefig_mock):
+def test_ply_input_for_05_icosahedron():
     f = open_string_as_file(ply_file_05_icosahedron)
     coordinates, edges, faces, edge_length_vec, file_name, \
         staple_name, singleXOs = ply_to_input(
-            "05_tetrahedron", f, min_len_nt=52)
+            "05_tetrahedron", f, min_len_nt=52, plot=False)
     assert len(coordinates) == 12
     assert len(faces) == 20
 
