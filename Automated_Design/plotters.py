60e0b67556ff07892fc8f82b863c2fcdccf97c07
Allen Grimm <TheGrimmScientist@gmail.com>
2017-03-16 16:54:51 -0700

    On edge length distributions, this makes the width of the bins are drawn as slightly increase to make bins still visible when there is a huge difference between the shortest and longest edge lengths.

--- Automated_Design/plotters.py
+++ Automated_Design/plotters.py
@@ -8,19 +8,30 @@ def plot_edge_length_distributions(shape_name,
                                    rounded_edge_length_PLY,
                                    results_foldername):
     min_len_nt = min(rounded_edge_length_PLY)
-    bins_for_hist = range(min_len_nt, max(rounded_edge_length_PLY) + 3, 1)
+    max_len_nt = max(rounded_edge_length_PLY)
+    bins_for_hist = range(min_len_nt, max_len_nt + 3, 1)
     bins_for_plotting = [x - 0.25 for x in bins_for_hist[:-1]]
 
-    # TODO: if there are too many bins (400-ish or more), then things get
-    # weird.  You can drastically increase the resolution to see what's going
-    # on.  Adding a bit more buffer on the left side of the graph helps, too.
-    # See sample shape 37_enneagonal_trapezohedron.ply as an example.
+    # Everything pertaining to `width_multiplier` is to help with the shapes
+    # with a very large difference between the largest edge lengths and the
+    # smallest edge lengths.  The general idea is you accept the possibility
+    # of overlapping bins (if there happen to be a few very similarly lengthed
+    # edges) to make the bins unfaithfully larger (that is, large enough to
+    # see on the graph).  This is all because the actual-width (as opposed to
+    # the width chosen to show the bins as) of the bins is important to keep
+    # at 1.
+    bin_width = 0.25  # for rounded bins, so half of non-rounded bins
+    bin_offset = float(bin_width)/2
+    width_multiplier = (1 + (max_len_nt - min_len_nt) / 75)
+
 
     fig_31 = plt.figure(0, figsize=(16, 8))
     fig_31.clf()
     y31 = np.histogram(scale_edge_length_PLY, bins=bins_for_hist)[0]
-    plt.bar(bins_for_plotting, y31, width=0.5)
-    plt.xlim((min_len_nt - 0.5, max(rounded_edge_length_PLY) + 1.5))
+    plt.bar([x + bin_width for x in bins_for_plotting],
+            y31, width=2 * bin_width * width_multiplier)
+    plt.xlim((min_len_nt - width_multiplier,
+              max_len_nt + 1.5 * width_multiplier))
     plt.ylim((0, max(y31) + 1))
     plt.title('Minimum edge length {} bp'.format(min_len_nt))
     plt.xlabel('Edge length (bp)')
@@ -30,12 +41,15 @@ def plot_edge_length_distributions(shape_name,
     fig_32 = plt.figure(1, figsize=(16, 8))
     fig_32.clf()
     y32 = np.histogram(rounded_edge_length_PLY, bins=bins_for_hist)[0]
-    bins_a = [x - 0.125 for x in bins_for_plotting]
-    bins_b = [x + 0.38 for x in bins_for_plotting]
-    plt.bar(bins_a, y31, width=0.25, color='b')
-    plt.bar(bins_b, y32, width=0.25, color='r')
+    bins_a = [x - (bin_offset * width_multiplier)
+              for x in bins_for_plotting]
+    bins_b = [x + bin_width + (bin_offset * width_multiplier)
+              for x in bins_for_plotting]
+    plt.bar(bins_a, y31, width=bin_width * width_multiplier, color='b')
+    plt.bar(bins_b, y32, width=bin_width * width_multiplier, color='r')
 
-    plt.xlim((min_len_nt - 0.5, max(rounded_edge_length_PLY) + 1.5))
+    plt.xlim((min_len_nt - width_multiplier,
+              max_len_nt + 1.5 * width_multiplier))
     plt.ylim((0, max(max(y31) + 1, 1)))
     plt.title('Edges rounded to nearest 10.5 bp')
     plt.xlabel('Edge length (bp)')
