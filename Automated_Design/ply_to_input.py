d2ef0b130113aa50b5c5a52a9bdf4c121e7add22
Allen Grimm <TheGrimmScientist@gmail.com>
2017-03-21 13:45:51 -0700

    Moved plotter for ply file edge lengths into ply_to_input.py to help with testing.  Marked that same plotter function as do-not-test.  Finished a proof of concept for setting up sets of tests using both a mocked function an example ply file.

--- Automated_Design/ply_to_input.py
+++ Automated_Design/ply_to_input.py
@@ -1,7 +1,7 @@
 from os import path
 import numpy as np
 
-from Automated_Design.plotters import plot_edge_length_distributions
+from matplotlib import pyplot as plt
 
 
 def extract_file_reader_and_shape_name_from_input_filename(input_filename):
@@ -63,7 +63,6 @@ def ply_to_input(input_filename, results_foldername=None, min_len_nt=31):
         `1` if single crossover vertex staples should be used,
         `0` if double crossover vertex staples should be used.
     """
-
 
     f, shape_name = extract_file_reader_and_shape_name_from_input_filename(
         input_filename)
@@ -213,3 +212,67 @@ def ply_to_input(input_filename, results_foldername=None, min_len_nt=31):
 
     return [coordinates, edges, faces, edge_length_vec, structure_name,
             staple_name, singleXOs]
+
+
+# note: this function is note covered under tests since I don't know how to
+# meaningfully test this primarily-aesthetics-oriented function.
+def plot_edge_length_distributions(shape_name,
+                                   scale_edge_length_PLY,
+                                   rounded_edge_length_PLY,
+                                   results_foldername):  # pragma: no cover
+    min_len_nt = min(rounded_edge_length_PLY)
+    max_len_nt = max(rounded_edge_length_PLY)
+    bins_for_hist = range(min_len_nt, max_len_nt + 3, 1)
+    bins_for_plotting = [x - 0.25 for x in bins_for_hist[:-1]]
+
+    # Everything pertaining to `width_multiplier` is to help with the shapes
+    # with a very large difference between the largest edge lengths and the
+    # smallest edge lengths.  The general idea is you accept the possibility
+    # of overlapping bins (if there happen to be a few very similarly lengthed
+    # edges) and make the bins unfaithfully larger (that is, large enough to
+    # see on the graph).  This is all because the actual-width (as opposed to
+    # the width chosen to show the bins as) of the bins is important to keep
+    # at 1, but you still want to be able to see the bins rather than an
+    # empty graph
+
+    bin_width = 0.25  # for rounded bins, so half of non-rounded bins
+    bin_offset = float(bin_width)/2
+    width_multiplier = (1 + (max_len_nt - min_len_nt) / 75)
+
+    fig_31 = plt.figure(0, figsize=(16, 8))
+    fig_31.clf()
+    y31 = np.histogram(scale_edge_length_PLY, bins=bins_for_hist)[0]
+    plt.bar([x + bin_width for x in bins_for_plotting],
+            y31, width=2 * bin_width * width_multiplier)
+    plt.xlim((min_len_nt - width_multiplier,
+              max_len_nt + 1.5 * width_multiplier))
+    plt.ylim((0, max(y31) + 1))
+    plt.title('Minimum edge length {} bp'.format(min_len_nt))
+    plt.xlabel('Edge length (bp)')
+    plt.ylabel('Number of edges')
+    # plt.xticks(bins_for_hist)
+
+    fig_32 = plt.figure(1, figsize=(16, 8))
+    fig_32.clf()
+    y32 = np.histogram(rounded_edge_length_PLY, bins=bins_for_hist)[0]
+    bins_a = [x - (bin_offset * width_multiplier)
+              for x in bins_for_plotting]
+    bins_b = [x + bin_width + (bin_offset * width_multiplier)
+              for x in bins_for_plotting]
+    plt.bar(bins_a, y31, width=bin_width * width_multiplier, color='b')
+    plt.bar(bins_b, y32, width=bin_width * width_multiplier, color='r')
+
+    plt.xlim((min_len_nt - width_multiplier,
+              max_len_nt + 1.5 * width_multiplier))
+    plt.ylim((0, max(max(y31) + 1, 1)))
+    plt.title('Edges rounded to nearest 10.5 bp')
+    plt.xlabel('Edge length (bp)')
+    plt.ylabel('Number of edges')
+    # plt.xticks(bins_for_hist)
+
+    shape_name_with_len = shape_name + '_{}_'.format(min_len_nt)
+    base_filename = path.join(results_foldername, shape_name_with_len)
+    fig_31.savefig(base_filename + 'min_edge_length_dist.png',
+                   bbox_inches='tight')
+    fig_32.savefig(base_filename + 'edges_rounded_to_10_5.png',
+                   bbox_inches='tight')
--- tests/test_ply_to_input.py
+++ tests/test_ply_to_input.py
@@ -1,6 +1,6 @@
 from StringIO import StringIO
 
-from mock import patch
+from mock import patch, MagicMock
 
 from Automated_Design.ply_to_input import ply_to_input
 
@@ -13,26 +13,6 @@ def open_string_as_file(string):
 
 
 # Sample files as strings:
-ply_file_01_tetrahedron = """
-ply
-format ascii 1.0
-element vertex 4
-property float32 x
-property float32 y
-property float32 z
-element face 4
-property list uint8 int32 vertex_indices
-end_header
-0.000000 0.000000 0.612372
--0.288675 -0.500000 -0.204124
--0.288675 0.500000 -0.204124
-0.577350 0.000000 -0.204124
-3 0 2 1
-3 0 1 3
-3 0 3 2
-3 1 2 3
-"""
-
 ply_file_05_icosahedron = """ply
 format ascii 1.0
 element vertex 12
@@ -77,19 +57,87 @@ end_header
 """
 
 
-@patch('Automated_Design.ply_to_input'
-       '.extract_file_reader_and_shape_name_from_input_filename')
-def test_ply_input_for_01_tetrahedron(extract_file_and_reader_mock):
-    f = open_string_as_file(ply_file_01_tetrahedron)
-    shape_name = '01_tetrahedron'
-    extract_file_and_reader_mock.return_value = [f, shape_name]
-
-    coordinates, edges, faces, edge_length_vec, file_name, staple_name, \
-        singleXOs = ply_to_input("01_tetrahedron", '', min_len_nt=52)
-    assert len(coordinates) == 4
-    assert len(faces) == 4
+# TODO: Add test for extract_file_reader_and_shape_name_from_input_filename()
 
 
+class TestPlyImportOnTetrahedron:
+    @classmethod
+    def setup_class(cls):
+        print("starting class: {} execution".format(cls.__name__))
+        cls.ply_file_01_tetrahedron = """
+            ply
+            format ascii 1.0
+            element vertex 4
+            property float32 x
+            property float32 y
+            property float32 z
+            element face 4
+            property list uint8 int32 vertex_indices
+            end_header
+            0.000000 0.000000 0.612372
+            -0.288675 -0.500000 -0.204124
+            -0.288675 0.500000 -0.204124
+            0.577350 0.000000 -0.204124
+            3 0 2 1
+            3 0 1 3
+            3 0 3 2
+            3 1 2 3
+        """
+        cls.shape_name = '01_tetrahedron'
+
+    def setup_method(self, method):
+        # You need to re-initialize the 'file reader' at the beginning
+        # of every function, else one test will read the file and the next
+        # test will begin reading the file at the end, find no data, and hang
+        # indefinitely.
+        f = open_string_as_file(self.ply_file_01_tetrahedron)
+
+        self.extract_file_and_reader_mock = patch(
+            'Automated_Design.ply_to_input'
+            '.extract_file_reader_and_shape_name_from_input_filename',
+            new=MagicMock(return_value=[f, self.shape_name]))
+        self.extract_file_and_reader_mock.start()
+
+    def teardown_method(self, method):
+        self.extract_file_and_reader_mock.stop()
+
+    def test_some_mundane_things(self):
+        coordinates, edges, faces, edge_length_vec, file_name, staple_name, \
+            singleXOs = ply_to_input("01_tetrahedron", '', min_len_nt=52)
+        assert len(coordinates) == 4
+        assert len(faces) == 4
+
+    # singleXOs tests:
+    def test_single_xos_are_not_used_when_min_len_nt_is_short(self):
+        coordinates, edges, faces, edge_length_vec, file_name, staple_name, \
+            singleXOs = ply_to_input("01_tetrahedron", '', min_len_nt=41)
+        assert singleXOs == 0
+
+    def test_single_xos_used_when_min_len_nt_is_long(self):
+        coordinates, edges, faces, edge_length_vec, file_name, staple_name, \
+            singleXOs = ply_to_input("01_tetrahedron", '', min_len_nt=43)
+        assert singleXOs == 1
+
+    # Plotting tests:
+    # On these two tests, you mock out the plotting function so only the mock
+    # and not the plotting function itself is called.  That way, the sample
+    # foldername defined in the second tests doesn't have to exist and you
+    # don't waste time re-generating plots every time you run tests.
+    @patch('Automated_Design.ply_to_input.plot_edge_length_distributions')
+    def test_plot_function_is_called_if_results_foldername_set(
+            self, plotter_mock):
+        ply_to_input("01_tetrahedron", results_foldername='')
+        assert plotter_mock.call_count == 0
+
+    @patch('Automated_Design.ply_to_input.plot_edge_length_distributions')
+    def test_plot_function_is_not_called_if_results_foldername_not_set(
+                self, plotter_mock):
+        ply_to_input("01_tetrahedron", results_foldername='i_am_defined')
+
+        assert plotter_mock.call_count == 1
+
+
+# A few small checks on a larger input file:
 @patch('Automated_Design.ply_to_input'
        '.extract_file_reader_and_shape_name_from_input_filename')
 def test_ply_input_for_05_icosahedron(extract_file_and_reader_mock):
