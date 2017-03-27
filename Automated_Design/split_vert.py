4b423731f220d2ef3d2cb3f89d054023a9bcd98c
Allen Grimm <TheGrimmScientist@gmail.com>
2017-03-27 11:21:41 -0700

    Updating split_vert docstring.

--- Automated_Design/split_vert.py
+++ Automated_Design/split_vert.py
@@ -2,28 +2,35 @@ from Automated_Design.arrange_neighbors import arrange_neighbors
 
 
 def split_vert(edge_type_mat_wHalfs, pseudo_vert, num_vert, vert_to_face):
-    # Splits vertices into N nodes, where N is the degree of the vertex
-    # Inputs: edge_type_mat_wHalfs = VxV sparse matrix where
-    #  -1 is half of a non-spanning tree edge (one side of scaffold crossover)
-    #   2 is spanning tree edge: DX edge with 0 scaffold crossovers
-    #         pseudo_vert = row vector where value j at index i indicate that
-    #            vertex i corresponds to vertex j, one of the V real vertices
-    #         num_vert = number of vertices, V
-    #         coordinates = Vx3 matrix of spatial coordinates of vertices
-    #         vert_to_face = Vx1 cell array, each row has a row vector listing
-    #             the face IDs the particular vertex belongs to
-    # Outputs: edge_type_mat_allNodes = sparse matrix where
-    #  -1 is half of a non-spanning tree edge (one side of scaffold crossover)
-    #   2 is spanning tree edge: DX edge with 0 scaffold crossovers
-    #        pseudo_vert = updated to include new pseudo-vertices
-    ###########################################################################
-    # by Sakul Ratanalert, MIT, Bathe Lab, 2016
-    #
-    # Copyright 2016. Massachusetts Institute of Technology. Rights Reserved.
-    # M.I.T. hereby makes following copyrightable material available to the
-    # public under GNU General Public License, version 2 (GPL-2.0). A copy of
-    # this license is available at https://opensource.org/licenses/GPL-2.0
-    ###########################################################################
+    """
+    Splits vertices into N nodes, where N is the degree of the vertex.
+
+    Parameters
+    ----------
+    edge_type_mat_wHalfs : networkx.classes.digraph.DiGraph
+        VxV sparse matrix where
+            -1 is half of a non-spanning tree edge (one side of scaffold
+            crossover)
+            2 is spanning tree edge: DX edge with 0 scaffold crossovers
+    pseudo_vert : list
+        row vector where value j at index i indicate that
+        vertex i corresponds to vertex j, one of the V real vertices
+    num_vert : int
+        number of vertices, V
+    vert_to_face : list
+        Vx1 cell array, each row has a row vector listing the face IDs the
+        particular vertex belongs to
+
+    Returns
+    -------
+    edge_type_mat_allNodes
+        sparse matrix where
+            -1 is half of a non-spanning tree edge (one side of scaffold
+            crossover)
+            2 is spanning tree edge: DX edge with 0 scaffold crossovers
+    pseudo_vert
+        updated to include new pseudo-vertices
+    """
 
     # # Initialize edge_type_mat_allNodes. It will be augmented from _wHalfs
     edge_type_mat_allNodes = edge_type_mat_wHalfs.copy()
