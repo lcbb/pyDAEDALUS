a8564f1653484455fbb0a3baa6ee9b3a21d6e920
Tyson <trsheph@mit.edu>
2018-04-10 12:04:44 -0400

    Removed folder 'Custom Sequence Generator'

--- Custom Sequence Generator/blastTest.sh
+++
@@ -1,2 +0,0 @@
-blastn -word_size 10 -query priTst.txt -subject customNCBI.txt -outfmt 6 > blastOut.txt
-blastn -word_size 10 -query priRev.txt -subject customNCBI.txt -outfmt 6 > blastOutRev.txt
--- Custom Sequence Generator/populate.sh
+++
@@ -1,2 +0,0 @@
-rm aPrimerCustomDB.db;
-./aPrimeDB.py; ./aPCRdecide_v2.py; ./blastTest.sh; ./expungeBadPrimers.py
